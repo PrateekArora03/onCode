8c9cb64d1e9bcff9f112d5c5583db49071737e2a
prarora98 <prateekisthebest98@gmail.com>
2019-09-09 10:59:22 +0530

    schema update

--- model/article.js
+++ model/article.js
@@ -7,7 +7,7 @@ const articleSchema = new Schema(
     post: { type: String, required: true, minlength: 10 },
     user: { type: Schema.Types.ObjectId, required: true },
     tags: { type: [Schema.Types.ObjectId] },
-    comment: { type: [Schema.Types.ObjectId], ref: "comment" },
+    comment: [{ type: Schema.Types.ObjectId, ref: "comment" }],
     views: { type: Number, default: 0 }
   },
   { timestamps: true }
--- model/tag.js
+++ model/tag.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const tagSchema = new Schema({
   name: { type: String, required: true, minlength: 3 },
   photo: { type: String, required: true },
-  post: { type: [Schema.Types.ObjectId], ref: "article" }
+  post: [{ type: Schema.Types.ObjectId, ref: "article" }]
 });
 
 const Tag = mongoose.model("Tag", tagSchema);
